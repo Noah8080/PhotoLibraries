9776ba834572bd6e1617ab0ced546dec44fe5d64
Noah Grenninger <nwg6@pct.edu>
2024-10-22 20:12:47 -0400

    Load images from local storage in a grid format

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,54 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View, FlatList, Text} from 'react-native';
+import { Image } from 'expo-image';
 import { ScreenContent } from '~/components/ScreenContent';
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+
+
+  const [permissionResponse, reqestPermission] = MediaLibrary.usePermissions();
+
+  const [localMedia, setLocalMedia] = useState<MediaLibrary.Asset[]>([]);
+
+  useEffect(() => {
+    // gets permission from user to access their local media library
+    if(permissionResponse?.status != 'granted') {
+      reqestPermission();
+    }
+  }, []);
+
+  // if permission is granted, load local media
+  useEffect(() => {
+    if(permissionResponse?.status === 'granted') {
+      loadLocalMedia();
+    }
+  }, [permissionResponse]);
+
+  const loadLocalMedia = async () => {
+    const mediaPage = await MediaLibrary.getAssetsAsync();
+    console.log(JSON.stringify(mediaPage, null, 2));
+    // set local media to the assets returned from the media library
+    setLocalMedia(mediaPage.assets);
+  };
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
+      <Stack.Screen options={{ title: 'Photos' }} />
+      {/* used to display the content in a list */}
+      <FlatList
+        columnWrapperStyle={{gap: 1}} // gap between columns
+        contentContainerStyle={{gap: 1}} // gap between rows
+        numColumns={4} // number of columns to display
+        data={localMedia} renderItem={({item}) => (
+           <Image source={{uri: item.uri}} 
+           style={{width: '33.3%', aspectRatio: 1 }} /> )} // width of 33.3% makes 3 columns and aspect ratio of 1 makes images square
+        />
+
+
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
+
