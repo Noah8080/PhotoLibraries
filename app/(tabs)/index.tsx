b50021935cd31f19cf58186ca0262aeb6898f1c0
Noah Grenninger <nwg6@pct.edu>
2024-10-23 21:38:51 -0400

    created file for global state for loading images. That way the methods for loading images can be called from any file in the project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,35 +4,13 @@ import { Image } from 'expo-image';
 import { ScreenContent } from '~/components/ScreenContent';
 import * as MediaLibrary from 'expo-media-library';
 import { useEffect, useState } from 'react';
+import { useMedia } from '~/providers/mediaProviders';
 
 export default function Home() {
+  const {assets, loadLocalMedia} = useMedia();
+  //console.log(abc);
 
-
-  const [permissionResponse, reqestPermission] = MediaLibrary.usePermissions();
-
-  const [localMedia, setLocalMedia] = useState<MediaLibrary.Asset[]>([]);
-
-  useEffect(() => {
-    // gets permission from user to access their local media library
-    if(permissionResponse?.status != 'granted') {
-      reqestPermission();
-    }
-  }, []);
-
-  // if permission is granted, load local media
-  useEffect(() => {
-    if(permissionResponse?.status === 'granted') {
-      loadLocalMedia();
-    }
-  }, [permissionResponse]);
-
-  const loadLocalMedia = async () => {
-    const mediaPage = await MediaLibrary.getAssetsAsync();
-    console.log(JSON.stringify(mediaPage, null, 2));
-    // set local media to the assets returned from the media library
-    setLocalMedia(mediaPage.assets);
-  };
-
+  
   return (
     <>
       <Stack.Screen options={{ title: 'Photos' }} />
@@ -40,12 +18,15 @@ export default function Home() {
       <FlatList
         columnWrapperStyle={{gap: 1}} // gap between columns
         contentContainerStyle={{gap: 1}} // gap between rows
-        numColumns={4} // number of columns to display
-        data={localMedia} renderItem={({item}) => (
+        numColumns={3} // number of columns to display
+        onEndReached={loadLocalMedia} // when the end of the list is reached, load the next page of media
+        onEndReachedThreshold={1}
+        //refreshing={load}
+        data={assets} 
+        renderItem={({item}) => (
            <Image source={{uri: item.uri}} 
            style={{width: '33.3%', aspectRatio: 1 }} /> )} // width of 33.3% makes 3 columns and aspect ratio of 1 makes images square
         />
-
 
     </>
   );
---
+++ providers/mediaProviders.tsx
@@ -0,0 +1,68 @@
+import { createContext, PropsWithChildren, useContext } from "react";
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect, useState } from 'react';
+
+type MediaContextType = {
+    assets: MediaLibrary.Asset[];
+    loadLocalMedia: () => void;
+};
+
+const MediaContext = createContext<MediaContextType>({
+    assets: [],
+    loadLocalMedia: () => {},
+});
+
+export default function MediaContextProvider({ children }: PropsWithChildren) {
+    
+  const [permissionResponse, reqestPermission] = MediaLibrary.usePermissions();
+  const [localMedia, setLocalMedia] = useState<MediaLibrary.Asset[]>([]);
+  // create varibles for finding end of page for local media, to be able to load more than one page of media
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [endCursor, setEndCursor] = useState<string>();
+  // make sure method can't be called while it is already loading
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // gets permission from user to access their local media library
+    if(permissionResponse?.status != 'granted') {
+        
+        reqestPermission();
+    }
+  }, []);
+
+  // if permission is granted, load local media
+  useEffect(() => {
+    if(permissionResponse?.status === 'granted') {
+      loadLocalMedia();
+    }
+  }, [permissionResponse]);
+
+  const loadLocalMedia = async () => {
+    if(loading || !hasNextPage) {
+      return;
+    }
+    setLoading(true);
+    console.log('loading local media');
+    const mediaPage = await MediaLibrary.getAssetsAsync({ after: endCursor }); // get the next page of media
+    console.log(JSON.stringify(mediaPage, null, 2));
+
+    // set local media to the assets returned from the media library
+    // if there is more than one page of media, this method is called again and will load the next page in front of the exisiting
+    setLocalMedia((existingItems) => [...existingItems, ...mediaPage.assets]);
+    setHasNextPage(mediaPage.hasNextPage);
+    setEndCursor(mediaPage.endCursor);
+    
+    setLoading(false);
+  };
+
+    return (
+        <MediaContext.Provider value={{assets: localMedia, loadLocalMedia}}>
+            {children}
+        </MediaContext.Provider>
+    )
+}
+
+
+export const useMedia = () => {
+   return useContext(MediaContext);
+}
