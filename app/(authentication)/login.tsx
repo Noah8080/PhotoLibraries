0f8e344f59d56f1eae797b47753f32697f13cad8
Noah Grenninger <nwg6@pct.edu>
2024-10-29 00:18:01 -0400

    added input validation for signUp button to make sure entered data passes desired email and password regex. Minor changes to styling in login/signUp page

--- app/(authentication)/login.tsx
+++ app/(authentication)/login.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { Alert, StyleSheet, View, AppState } from 'react-native'
+import { Alert, StyleSheet, View, AppState, Image } from 'react-native'
 import { supabase } from '~/utils/supabase'
 import { Button, Input } from '@rneui/themed'
+//import Logo from '~/assets/icon-logo.png'
 
 // Tells Supabase Auth to continuously refresh the session automatically if
 // the app is in the foreground. When this is added, you will continue to receive
@@ -20,6 +21,68 @@ export default function Auth() {
   const [password, setPassword] = useState('')
   const [username, setUsername] = useState('')
   const [loading, setLoading] = useState(false)
+
+
+  // creat function for validating user's input before calling the sign up or sign in functions
+  const signUpPressed = () => {
+
+    validateInput();
+
+  }
+
+  /**
+   * validates the user's input before calling the sign in function
+   */
+  const validateInput = () => { 
+    // create an array to hold error messages
+    let errors = [];
+
+    let isEmail = false;
+    let isPassword = false;
+    // the supabase API uses parameterized queries to prevent SQL injection attacks, but we will still validate the user's input
+    let emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ 
+    if (emailReg.test(email) === true) {
+      console.log('Email is valid: ' + email);
+      isEmail = true;
+    }
+    else {
+      console.log('Invalid email format: ' + email);
+      errors.push('Email is invalid');
+      isEmail = false;
+    }
+
+    // ------validate password----
+        // check if the password is valid
+        // password must be at least 8 characters long
+        // password must contain at least one number
+        // password must contain at least one uppercase letter
+        // password must contain at least one lowercase letter
+        // password must contain at least one special character
+        let passwordReg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+        if (passwordReg.test(password) === true){
+            console.log('password is valid: ' + password);
+            isPassword = true;
+        }
+        else{
+            console.log('password is not valid: ' + password);
+            isPassword = false;
+            errors.push('\n \npassword must be at least 8 characters long, have no spaces, contain one uppercase letter, one lowercase letter, at least one number, and one special character');
+        }
+
+        // if the email and password are valid, call the sign up function
+        if (isEmail && isPassword) {
+            console.log('inputs were valid');
+            signUpWithEmail();
+        }
+        else {
+            // if the email or password are not valid, display the error messages
+            alert(errors);
+        }
+
+  }
+
+
 
   /**
    * function for a returning user to sign in
@@ -63,7 +126,8 @@ export default function Auth() {
   return (
     <View style={styles.container}>
       <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
+
+        <Input style={styles.imBorders}
           label="Email"
           leftIcon={{ type: 'font-awesome', name: 'envelope' }}
           onChangeText={(text) => setEmail(text)}
@@ -82,14 +146,19 @@ export default function Auth() {
           secureTextEntry={true}
           placeholder="Password"
           autoCapitalize={'none'}
+          className='rounded-md boder border'
+
         />
       </View>
       <View style={[styles.verticallySpaced, styles.mt20]}>
         <Button title="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
       </View>
       <View style={styles.verticallySpaced}>
-        <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
+        <Button title="Sign up"  disabled={loading} onPress={() => signUpPressed()} />
+
       </View>
+      {/* <Image source={Logo} style={[styles.logo, ]}  /> */}
+
     </View>
   )
 }
@@ -98,18 +167,34 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 40,
     padding: 12,
+    alignContent: 'center',
+    backgroundColor: 'pink',
+    width: '100%',
+    height: '100%',
   },
   verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
+    paddingTop: 3,
+    paddingBottom: 3,
     alignSelf: 'stretch',
   },
   mt20: {
     marginTop: 20,
   },
   imBorders: {
-    borderWidth: 1,
+    borderWidth: 1.5,
     borderColor: 'black',
-  }
+    backgroundColor: 'lightgray',
+  },
+
+  logo: {
+    width: '75%',
+    height: 200,
+    marginTop: 40,
+    marginBottom: 10,
+    alignContent: 'center',
+    marginLeft: 80,
+    aspectRatio: 1,
+
+},
   
 })
